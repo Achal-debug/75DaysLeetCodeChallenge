f4d76d147ff343cb4d6a3b0ac042e1268f7ac8af
Achal-debug <achalgupta25000@gmail.com>
2026-03-18 18:15:25 +0530

    Time: 0 ms (100%), Space: 40.2 MB (58.9%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,21 @@
+// Day 7 - 75 Days LeetCode Challenge
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n, 1);
+        // Prefix
+        int prefix = 1;
+        for(int i = 0; i < n; i++){
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+        // Suffix
+        int suffix = 1;
+        for(int i = n - 1; i >= 0; i--){
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return result;
+    }
+};
