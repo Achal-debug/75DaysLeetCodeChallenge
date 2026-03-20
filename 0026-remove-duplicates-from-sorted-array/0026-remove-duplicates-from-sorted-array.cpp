ca528956e11d283f7d89118e8a324a3b9688586f
Achal-debug <achalgupta25000@gmail.com>
2026-03-20 11:09:35 +0530

    Time: 0 ms (100%), Space: 22.7 MB (52.15%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,15 @@
+// Day 9 - 75 Days LeetCode Challenge
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        int j = 0;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] != nums[j]){
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+};
