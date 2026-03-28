03509248ad70a102bb7f5b0bf67919917bf496e3
Achal-debug <achalgupta25000@gmail.com>
2026-03-28 19:18:48 +0530

    Time: 2 ms (70.21%), Space: 10.9 MB (39.75%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,19 @@
+// Day 17 - 75 Days LeetCode Challenge 
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> count(26, 0);
+        int left = 0, maxFreq = 0, maxLen = 0;
+        for(int right = 0; right < s.length(); right++){
+            count[s[right] - 'A']++;
+            maxFreq = max(maxFreq, count[s[right] - 'A']);
+            // check condition
+            while((right - left + 1) - maxFreq > k){
+                count[s[left] - 'A']--;
+                left++;
+            }
+            maxLen = max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+};
