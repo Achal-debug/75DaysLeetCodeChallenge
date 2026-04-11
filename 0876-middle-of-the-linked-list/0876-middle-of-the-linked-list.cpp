1ae00a0f896e75113c767e01f20e66cdd5c96ef2
Achal-debug <achalgupta25000@gmail.com>
2026-04-11 17:36:48 +0530

    Time: 0 ms (100%), Space: 10 MB (25.58%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -0,0 +1,15 @@
+// Day 31 - 75 Days LeetCode Challenge 
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
