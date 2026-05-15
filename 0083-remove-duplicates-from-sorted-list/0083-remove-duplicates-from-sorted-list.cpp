b9fa1b543aea23b146f53deb677eefc25beba412
Achal-debug <achalgupta25000@gmail.com>
2026-05-15 11:56:32 +0530

    Time: 0 ms (100%), Space: 16.3 MB (33.04%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,15 @@
+// Day 65 - 100 Days LeetCode Challenge
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* current = head;
+        while(current != NULL && current->next != NULL){
+            if(current->val == current->next->val){
+                current->next = current->next->next;
+            } else {
+                current = current->next;
+            }
+        }
+        return head;
+    }
+};
