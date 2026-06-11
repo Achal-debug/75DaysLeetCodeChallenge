4402f59bbe3ecabe2ff088427d1ae4e1802c71fa
Achal-debug <achalgupta25000@gmail.com>
2026-06-11 15:07:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (10.44%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,12 @@
+// Day 92 - 100 Days LeetCode Challenge
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while(n) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+};
